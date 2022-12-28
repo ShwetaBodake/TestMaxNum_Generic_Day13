c0b034ce946da0b010fe37b8ec8403683d5a65a3
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-28 23:09:38 +0530

    Solution of finding Maximum Integer number

---
+++ MaximumIntegerNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace GenericTestMaximumNumber
+{
+	public class MaximumIntegerNumber
+	{
+
+        public static int MaximumNumber(int firstValue, int secondvalue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondvalue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondvalue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondvalue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if (secondvalue.CompareTo(firstValue) > 0 && secondvalue.CompareTo(thirdValue) > 0 ||
+                secondvalue.CompareTo(firstValue) >= 0 && secondvalue.CompareTo(thirdValue) > 0 ||
+                secondvalue.CompareTo(firstValue) > 0 && secondvalue.CompareTo(thirdValue) >= 0)
+            {
+                return secondvalue;
+            }
+
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondvalue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondvalue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondvalue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            return 0;
+
+        }
+    }
+}
+
