fd507f3b1ef471d5a76fef52367235f52d22c423
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-28 22:59:42 +0530

    Main method Of finding max number to call static methodg

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace GenericTestMaximumNumber
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int firstValue = 22;
+            int secondvalue = 33;
+            int thirdValue = 22;
+
+            int output = MaximumIntegerNumber.MaximumNumber(firstValue, secondvalue, thirdValue);
+            Console.WriteLine("Maximum Integer Number is: " + output);
+
+
+        }
+    }
+}
