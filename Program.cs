1507cfa6b5a7521b85b175c2299ddc0384beba90
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-29 11:33:05 +0530

    Generic Method calling

--- Program.cs
+++ Program.cs
@@ -8,14 +8,33 @@ namespace GenericTestMaximumNumber
     {
         public static void Main(string[] args)
         {
-            int firstValue = 22;
-            int secondvalue = 33;
-            int thirdValue = 22;
+           
+            RefactorCodeMaxNum obj1 = new RefactorCodeMaxNum();
 
-            int output = MaximumIntegerNumber.MaximumNumber(firstValue, secondvalue, thirdValue);
-            Console.WriteLine("Maximum Integer Number is: " + output);
+            int result=obj1.MaxNum<int>(21,33,44);
 
+            Console.WriteLine(result);
+
+
+//----------------------------------------------------------------------------------------------//
+           //UsingGenericMaxNumber<int> obj = new UsingGenericMaxNumber<int>();
+            //obj.GenericMaxNumber(firstValue,secondvalue,thirdValue);
+
+
+            //    double firstValue = 23.45;
+            //    double secondvalue = 12.32;
+            //    double thirdValue = 23.44;
+
+
+            //MaximumFloatNumber.fMaximumNumber(firstValue, secondvalue, thirdValue);
+            //Console.WriteLine(MaximumFloatNumber.fMaximumNumber(firstValue, secondvalue, thirdValue));
+
+            //string firstValue = "shweta";
+            //string secondvalue = "Prithvi";
+            //string thirdValue = "Hrishikesh";
+            //MaximumString.Maximumstring(firstValue, secondvalue, thirdValue);
+            //    Console.WriteLine(MaximumString.Maximumstring(firstValue, secondvalue, thirdValue));
 
         }
-    }
+    } 
 }
