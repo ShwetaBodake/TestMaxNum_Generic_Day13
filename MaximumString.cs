fa4b4e3a954c66bf7859861c97c3eb5c1f47bdf3
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-29 10:31:58 +0530

    Solution for maximum string

---
+++ MaximumString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace GenericTestMaximumNumber
+{
+	public class MaximumString
+	{
+
+        public static string Maximumstring(string firstValue, string secondvalue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondvalue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondvalue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondvalue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+
+            if (secondvalue.CompareTo(firstValue) > 0 && secondvalue.CompareTo(thirdValue) > 0 ||
+                secondvalue.CompareTo(firstValue) >= 0 && secondvalue.CompareTo(thirdValue) > 0 ||
+                secondvalue.CompareTo(firstValue) > 0 && secondvalue.CompareTo(thirdValue) >= 0)
+            {
+                return secondvalue;
+            }
+
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondvalue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondvalue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondvalue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            return default;
+        }
+
+    }
+}
+
