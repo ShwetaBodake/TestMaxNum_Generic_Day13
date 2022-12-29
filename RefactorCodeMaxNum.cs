c970b12c45502e535723abf81d2e7bd222157414
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-29 11:37:42 +0530

    Refactor code with generic to find Maximum

---
+++ RefactorCodeMaxNum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace GenericTestMaximumNumber
+{
+	public class RefactorCodeMaxNum 
+    {
+		public T MaxNum<T>(T firstValue,T secondValue, T thirdValue) where  T : IComparable
+		{
+
+			if (firstValue.CompareTo(secondValue )>0 && firstValue.CompareTo(thirdValue)>0)
+			{
+				return firstValue;
+			}
+			else if (secondValue.CompareTo(firstValue)>0 && secondValue.CompareTo(thirdValue)>0)
+			{
+				return secondValue;
+			}
+			else if (thirdValue.CompareTo(firstValue)>0 && thirdValue.CompareTo(secondValue)>0)
+			{
+				return thirdValue;
+			}
+			else
+				return firstValue;
+
+		}
+	}
+}
+
